bb150708e3404d71a7142916826d16797450a28e
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-17 14:28:08 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,13 +9,14 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, flag = 0;
 	int tmp;
 
 	if (size < 1)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,8 +24,11 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+				flag++;
 				print_array(array, size);
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
