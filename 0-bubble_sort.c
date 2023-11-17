266c47a13127298480776b2b4d3ed3633ec3fa8b
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-17 13:54:25 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, flag = 0;
+	int tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -17,9 +18,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				array[j] ^= array[j + 1];
-				array[j + 1] = array[j] ^ array[j + 1];
-				array[j] ^= array[j + 1];
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				flag++;
 				print_array(array, size);
 			}
